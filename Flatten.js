577e72680c86a8e2b991cf96b87c7262ebaf3f7f
Raghavendra M <32561508+Js4Life@users.noreply.github.com>
2021-08-23 14:05:34 +0530

    Create Flatten.js

---
+++ Flatten.js
@@ -0,0 +1,10 @@
+[1,[2,3]].join().split(',').map(Number);
+
+[1,[2,3]].join().split(',').map(Number);
+(3) [1, 2, 3]0: 11: 22: 3length: 3[[Prototype]]: Array(0)
+[1,[2,3]].join()
+"1,2,3"
+[1,[2,3]].join().split(',')
+(3) ["1", "2", "3"]
+[1,[2,3]].join().split(',').map(Number);
+(3) [1, 2, 3]
